1712822c98726033b325f35c02826227f01849f3
Lasisa <133031703+Lasisa@users.noreply.github.com>
2023-06-09 10:12:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+/*
+
+
+
+
+for(let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+
+
+
+
+for(let i = 55; i >= 20; i--) {
+    console.log(i)
+}
+
+
+
+
+
+const numbers = [3, 5, 11, 2, 8, 1,6]
+for(let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+
+const numbers = [3, 5, 11, 2, 8, 1, 6]
+const numbers_squared = []
+
+for(let i = 0; i < numbers.length; i++) {
+    numbers_squared.push(numbers[i] ** 2)
+}
+console.log(numbers_squared)
+
+
+
+
+const last_elem = numbers_squared[numbers_squared.length - 1]
+console.log(last_elem)
+
+
+
+const user = {
+    first_name: "Ivan",
+    last_name: "Ivanov",
+    age: 20,
+    salary:500
+}
+const formattedString = `Users name is ${user.first_name} ${user.last_name}. He is ${user.age} year old`
+console.log(formattedString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
